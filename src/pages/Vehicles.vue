dc903016fb7737866ac1726292dff3640ecfacc0
Darya Yusipets <dyusipets@gomel.iba.by>
2022-06-29 18:06:48 +0300

    completed filters block

--- src/pages/Vehicles.vue
+++ src/pages/Vehicles.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="filters">
-        <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select class" />
-        <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select gearbox type" />
-        <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select brand" />
-        <Dropdown v-model="selectedCity" :options="cities" optionLabel="name" placeholder="Select year" />
+        <Dropdown v-model="selectedClass" :options="classesList" placeholder="Select class" />
+        <Dropdown v-model="selectedGearboxType" :options="gearboxTypes" placeholder="Select gearbox type" />
+        <Dropdown v-model="selectedBrand" :options="brandsList" placeholder="Select brand" />
+        <Dropdown v-model="selectedYear" :options="years" placeholder="Select year" />
         <span class="p-input-icon-left">
             <i class="pi pi-search" />
             <InputText type="text" v-model="search" placeholder="Search" class="search" />
@@ -24,21 +24,27 @@ export default {
     components: { CarCard },
     data: () => ({
         cars: [],
+        brandsList: [],
+        classesList: [],
+        gearboxTypes: [],
+        years: [],
         search: "",
         price: 250,
-        selectedCity: null,
-        cities: [
-            { name: 'New York', code: 'NY' },
-            { name: 'Rome', code: 'RM' },
-            { name: 'London', code: 'LDN' },
-            { name: 'Istanbul', code: 'IST' },
-            { name: 'Paris', code: 'PRS' }
-        ],
-        value3: "",
+        selectedBrand: null,
+        selectedClass: null,
+        selectedGearboxType: null,
+        selectedYear: null,
+        any: "<any>"
     }),
     mounted() {
         axios.get("http://localhost:4000/cars")
-            .then(res => this.cars = res.data)
+            .then(res => {
+                this.cars = res.data;
+                this.brandsList = [this.any, ...new Set(res.data.map(car => car.brand))];
+                this.classesList = [this.any, ...new Set(res.data.map(car => car.class))];
+                this.gearboxTypes = [this.any, ...new Set(res.data.map(car => car.gearbox))];
+                this.years = [this.any, ...new Set(res.data.map(car => car.year))].sort((a, b) => b - a);
+            })
             .catch(err => console.log(err));
     },
     // methods: {
@@ -56,9 +62,25 @@ export default {
         maxPrice() {
             return Math.max(...this.cars.map(car => car.price));
         },
-        filteredResult() {
+        filteredByPrice() {
             return this.searchResult.filter(car => car.price <= this.price);
         },
+        filteredByBrand() {
+            if (!this.selectedBrand || this.selectedBrand === this.any) return this.filteredByPrice;
+            return this.filteredByPrice.filter(car => car.brand === this.selectedBrand);
+        },
+        filteredByClass() {
+            if (!this.selectedClass || this.selectedClass === this.any) return this.filteredByBrand;
+            return this.filteredByBrand.filter(car => car.class === this.selectedClass);
+        },
+        filteredByYear() {
+            if (!this.selectedYear || this.selectedYear === this.any) return this.filteredByClass;
+            return this.filteredByClass.filter(car => car.year === this.selectedYear);
+        },
+        filteredResult() {
+            if (!this.selectedGearboxType || this.selectedGearboxType === this.any) return this.filteredByYear;
+            return this.filteredByYear.filter(car => car.gearbox === this.selectedGearboxType);
+        }
     }
 }
 </script>
